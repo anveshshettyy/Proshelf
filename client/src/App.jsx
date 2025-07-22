4c90629886a7f096dd19b6367a5eaf729fd71dbc
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-07-22 20:25:53 +0530

    frontend fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Collections from './Pages/Collections'
 import ProtectedRoute from './Components/ProtectedRoute'
 import Login from './Pages/Login'
