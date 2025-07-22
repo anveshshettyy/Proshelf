d48eae42a65c723f756bf5f03864722855e6042d
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-07-22 19:37:29 +0530

    commit

--- client/src/Context/AuthContext.jsx
+++ client/src/Context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import axiosInstance from '../../lib/axios';
+import axiosInstance from '../lib/axios';
 
 const AuthContext = createContext();
 
